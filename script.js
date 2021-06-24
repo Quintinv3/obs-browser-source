f9cba2738a65dba5efa47329c216011e28423aad
Quintinv3 <70272776+Quintinv3@users.noreply.github.com>
2021-06-24 20:00:38 +0200

    v2

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-"use strict";
-let clearField = function clearField() {
-  document.getElementById("input").value = "";
-};
-
-document.addEventListener(`keydown`, function (e) {
-  if (e.key === `Enter`) {
-    var x = document.getElementById("input");
-    var defaultVal = x.defaultValue;
-    var currentVal = x.value;
-
-    defaultVal = currentVal;
-  }
-  console.log(defaultVal);
-});
-console.log(document.getElementById("input").defaultValue);
+function submit() {
+  let inputText = document.getElementById("input").value;
+  let outputText = (document.getElementById("output").value = inputText);
+}
